3ffd7a22454235cbdb0890f141a1d0c99f0eee45
MatsumaruTsuyoshi <strivesdfpx379@gmail.com>
2024-02-19 15:31:45 +0900

    Initial Commit

---
+++ cal_tax_swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cal_tax_swiftui
+//
+//  Created by matsumarutsuyoshi on 2024/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cal_tax_swiftui/cal_tax_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cal_tax_swiftuiApp.swift
+//  cal_tax_swiftui
+//
+//  Created by matsumarutsuyoshi on 2024/02/19.
+//
+
+import SwiftUI
+
+@main
+struct cal_tax_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
